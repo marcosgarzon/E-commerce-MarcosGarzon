b71732142e758855a96d68c37dc38f79fb71d1b4
Marcos Garzon <marcosgarzonmdq@gmail.com>
2022-04-26 19:57:09 -0300

    ItemCount 1.1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,7 +3,7 @@ import ItemCount from '../ItemCount/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
   return (
-    <div className='container'><h2 className='bg-primary p-10'>{greeting}</h2><ItemCount parrafo={"Espada de tipo colosal utilizada antiguamente por la élite de origen noble"} texto="Espadón de noble" stockItem="7" inicialContador="0"/></div>
+    <div className='container'><h2 className='bg-primary p-10'>{greeting}</h2><ItemCount parrafo={"Espada de tipo colosal utilizada antiguamente por la élite de origen noble"} texto="Espadón de noble" stockItem="7" inicialContador="1"/></div>
   )
 }
 
